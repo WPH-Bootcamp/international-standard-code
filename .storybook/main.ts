43c3d8be64ac100e6c7401433eba74c8b29ee4b2
Kyra <ramadhanky68@gmail.com>
2025-10-11 19:18:50 +0700

    feature/storybook for components documentations

---
+++ .storybook/main.ts
@@ -0,0 +1,28 @@
+import type { StorybookConfig } from '@storybook/react-vite';
+import { resolve } from 'path';
+import { mergeConfig } from 'vite';
+
+const config: StorybookConfig = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@storybook/addon-essentials',
+    '@chromatic-com/storybook',
+    '@storybook/addon-docs',
+    '@storybook/addon-a11y',
+    '@storybook/addon-vitest',
+  ],
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
+  },
+  viteFinal: async (config) => {
+    return mergeConfig(config, {
+      resolve: {
+        alias: {
+          '@': resolve(__dirname, '../src'),
+        },
+      },
+    });
+  },
+};
+export default config;
---
+++ src/components/ui/Input/Input.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Input: React.FC = (props) => {
+  return <input {...props} />;
+};
+
+export default Input;
